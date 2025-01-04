fbde370f01d0283cb3a9cd620d9e0345a4233ef1
rene3phillips <rene3phillips@gmail.com>
2025-01-04 10:01:31 -0500

    Change times

--- main.py
+++ main.py
@@ -31,10 +31,10 @@ def follow_with_log():
     log_activity("Followed authors from feed")  # Log the activity of following authors
 
 # Schedule tasks
-schedule.every().day.at("14:10").do(post_text_with_log)  
-schedule.every().day.at("14:11").do(post_image_with_log)  
-schedule.every().day.at("14:12").do(like_posts_with_log) 
-schedule.every().day.at("14:13").do(follow_with_log)  
+schedule.every().day.at("08:00").do(post_text_with_log)  
+schedule.every().day.at("08:01").do(post_image_with_log)  
+schedule.every().day.at("08:02").do(like_posts_with_log) 
+schedule.every().day.at("08:03").do(follow_with_log)  
 
 print("All tasks have been scheduled.")  # Print confirmation that all tasks are scheduled
 
